5fbbba7de7d9726cefc6552866d33a582d93034b
Johnanater <10360610+Johnanater@users.noreply.github.com>
2020-01-19 19:35:11 -0800

    1.0.1 - Fix multithreaded save
    Fixes Nelson's new protection on only saving from the main Unity thread. Also technically a better way than making a new thread. :)

--- Main.cs
+++ Main.cs
@@ -1,9 +1,10 @@
 ﻿using System;
-using System.Threading;
+using System.Collections;
 using Rocket.API.Collections;
-using Rocket.Core.Logging;
 using Rocket.Core.Plugins;
 using SDG.Unturned;
+using UnityEngine;
+using Logger = Rocket.Core.Logging.Logger;
 
 namespace SaveCrash
 {
@@ -12,40 +13,47 @@ namespace SaveCrash
         public static Main Instance;
         public static Configuration Config;
 
+        public const string Version = "1.0.1";
+
         protected override void Load()
         {
             Config = Configuration.Instance;
+            
+            Logger.Log($"SaveCrash by Johnanater, version: {Version}");
+            Logger.Log(Translate("checking_in", Config.CheckTime));
+            
+            StartCoroutine(CheckCoroutine());
+        }
+
+        public IEnumerator CheckCoroutine()
+        {
+            yield return new WaitForSeconds(Config.CheckTime);
+            
             CheckSave();
         }
 
         void CheckSave()
         {
-            new Thread(() =>
+            Logger.Log(Translate("try_save"));
+            try
             {
-                Thread.Sleep(Config.CheckTime * 1000);
-                Logger.Log(Translate("try_save"));
-                try
-                {
-                    SaveManager.save();
-                    Logger.Log(Translate("save_success"));
-                }
-                catch (Exception e)
-                {
-                    Logger.Log(Translate("save_failed"));
-                    Logger.Log(e);
-                    Provider.shutdown(Config.ShutdownTime);
-                }
-            })
+                SaveManager.save();
+                Logger.Log(Translate("save_success"));
+            }
+            catch (Exception e)
             {
-                IsBackground = true
-            }.Start();
+                Logger.Log(Translate("save_failed"));
+                Logger.Log(e);
+                Provider.shutdown(Config.ShutdownTime);
+            }
         }
 
         public override TranslationList DefaultTranslations => new TranslationList()
         {
             {"try_save", "Let's try saving!"},
             {"save_success", "Save successful! We're good to go!"},
-            {"save_failed", "Save failed! Restarting the server!"}
+            {"save_failed", "Save failed! Restarting the server!"},
+            {"checking_in", "Checking save in {0} seconds!"}
         };
     }
 }
